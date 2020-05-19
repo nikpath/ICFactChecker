9bd935d2168d64323105e7b2b5aec4802e5c4bb8
nikpath <31492570+nikpath@users.noreply.github.com>
2020-05-19 17:06:44 -0400

    Create background.js

---
+++ background.js
@@ -0,0 +1,10 @@
+  {
+    "name": "ICFactChecker",
+    "version": "1.0",
+    "description": "Check yo facts, yo",
+    "background": {
+      "scripts": ["background.js"],
+      "persistent": false
+    },
+    "manifest_version": 2
+  }

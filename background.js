5a3554cc586d5ccffeea3bf83b1ebe21eb5ec8c2
nikpath <niki.pathak18@gmail.com>
2020-05-20 19:04:08 -0400

    Created menu option which is only activated when words are selected and then right clicked. Currently the onclick event just displays the selected text

--- background.js
+++ background.js
@@ -1,1 +1,14 @@
 //background.js
+var contexts = ["selection"];
+
+chrome.contextMenus.create({
+  "contexts": contexts,
+  id: 'menuTitle',
+  title: 'Fact check this'
+});
+
+function onClickHandler(info, tab){
+  alert(info.selectionText);
+}
+
+chrome.contextMenus.onClicked.addListener(onClickHandler);

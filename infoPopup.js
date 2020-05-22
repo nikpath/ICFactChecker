d988724bbe3ce152a1d492be877fb4278e13ad52
nikpath <niki.pathak18@gmail.com>
2020-05-22 16:48:00 -0400

    Updated iframe url source to be the search results for factcheck.org

--- infoPopup.js
+++ infoPopup.js
@@ -4,19 +4,20 @@ chrome.runtime.onMessage.addListener(
     window.searchWords = request.searchWords;
     if (request.searchWords){
       sendResponse({farewell: searchWords});
-
     }
   });
 
-  // Avoid recursive frame insertion...
-  var extensionOrigin = 'chrome-extension://' + chrome.runtime.id;
-  if (!location.ancestorOrigins.contains(extensionOrigin)) {
-      var iframe = document.createElement('iframe');
-      // Must be declared at web_accessible_resources in manifest.json
-      iframe.src = chrome.runtime.getURL('infoPopup.html');
+  if(searchWords){
+      // Avoid recursive frame insertion...
+      var extensionOrigin = 'chrome-extension://' + chrome.runtime.id;
+      if (!location.ancestorOrigins.contains(extensionOrigin)) {
+          var iframe = document.createElement('iframe');
+          // Must be declared at web_accessible_resources in manifest.json
+          iframe.src = "https://factcheck.org/search/?q=" + searchWords;
 
-      // Some styles for a fancy sidebar
-      iframe.style.cssText = 'position:fixed;top:0;left:0;display:block;' +
-                             'width:300px;height:30%;z-index:1000;';
-      document.body.appendChild(iframe);
+          // Some styles for a fancy sidebar
+          iframe.style.cssText = 'position:fixed;bottom:0;right:0;display:block;' +
+                                 'width:300px;height:30%;z-index:1000;';
+          document.body.appendChild(iframe);
+      }
   }

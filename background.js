8023febc28f284e2a43b1f5590c1a47d45fa52be
s6sarwar <s6sarwar@edu.uwaterloo.ca>
2020-05-22 17:32:39 -0400

    hi

--- background.js
+++ background.js
@@ -1,1 +1,28 @@
+<<<<<<< Updated upstream
 //background.js
+=======
+//background.js
+var contexts = ["selection"];
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.contextMenus.create({
+    "contexts": contexts,
+    id: 'menuTitle',
+    title: 'Fact check this'
+  });
+});
+
+chrome.contextMenus.onClicked.addListener(function (item, tab) {
+  var searchWords = item.selectionText;  
+
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) { 
+  	chrome.tabs.executeScript({
+          file: 'infoPopup.js'
+  });
+  chrome.tabs.sendMessage(tabs[0].id, {searchWords: searchWords}, function(response) {
+    console.log(response.farewell);
+  });
+});
+
+});
+>>>>>>> Stashed changes
---
+++ infoPopup.js
@@ -0,0 +1,23 @@
+var searchWords;
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    window.searchWords = request.searchWords;
+    if (request.searchWords){
+      sendResponse({farewell: searchWords});
+    }
+  });
+
+  if(searchWords){
+      // Avoid recursive frame insertion...
+      var extensionOrigin = 'chrome-extension://' + chrome.runtime.id;
+      if (!location.ancestorOrigins.contains(extensionOrigin)) {
+          var iframe = document.createElement('iframe');
+          // Must be declared at web_accessible_resources in manifest.json
+          iframe.src = "https://factcheck.org/search/?q=" + searchWords;
+
+          // Some styles for a fancy sidebar
+          iframe.style.cssText = 'position:fixed;bottom:0;right:0;display:block;' +
+                                 'width:300px;height:100%;z-index:1000;';
+          document.body.appendChild(iframe);
+      }
+  }
